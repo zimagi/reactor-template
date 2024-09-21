9201da03fdb9befeaaa56d85a286d449e0fe4441
Adrian Webb <adrian@webb.sh>
2024-09-21 02:00:09 -0400

    Removing the unused variables in the pre gen project hook.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -14,18 +14,6 @@ INFO = "\x1b[1;33m [INFO]: "
 HINT = "\x1b[3;33m"
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 
-# The content of this string is evaluated by Jinja, and plays an important role.
-# It updates the cookiecutter context to trim leading and trailing spaces
-# from domain/email values
-"""
-{{ cookiecutter.update({ "domain_name": cookiecutter.domain_name | trim }) }}
-{{ cookiecutter.update({ "from_email": cookiecutter.from_email | trim }) }}
-{{ cookiecutter.update({ "contact_email": cookiecutter.contact_email | trim }) }}
-{{ cookiecutter.update({ "mailgun_api_key": cookiecutter.mailgun_api_key | trim }) }}
-{{ cookiecutter.update({ "mailgun_webhook_key": cookiecutter.mailgun_webhook_key | trim }) }}
-{{ cookiecutter.update({ "mailgun_domain": cookiecutter.mailgun_domain | trim }) }}
-"""
-
 project_slug = "{{ cookiecutter.project_slug }}"
 if hasattr(project_slug, "isidentifier"):
     assert project_slug.isidentifier(), "'{}' project slug is not a valid Python identifier.".format(project_slug)
